9cc128c94c77407d9340140391118f98eccf0e66
Denis <kalimm@bk.ru>
2022-11-27 13:53:56 +0300

    Adding logo and search bar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,52 @@
-<div class="header">
-    <div class="header-bot">
-        
-        <div class="wrap">
-            <div class="header-bot1">
-                <div class="logo">
-                    {{-- <a href="index.html"><img src="/public/assets/user/images/logo.png" alt=""></a> --}}
-                    <h1 class="fw-bold fs-1">Mychord.com</h1>
-                </div>
-                <div class="ph-no">
-                    <div class="search_box">
-                        <form method="get" action="{{ route('search') }}">
-                            <input type="text" name="s" value="" required>
-                                <input type="submit"
-                                value="">
-                        </form>
+
+<header>
+    <!-- Header Start -->
+   <div class="header-area">
+        <div class="main-header ">
+           
+           <div class="header-bottom header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-10 col-lg-10 col-md-12 header-flex">
+                            
+                            <!-- Main-menu -->
+                            <div class="main-menu d-none d-md-block">
+                                <nav>                  
+                                    <ul id="navigation">  
+                                        <li><a href="index.html"><img class="" width="200" src="/public/assets/img/logo/logo1.png" alt=""></a></li> 
+                                        <li><a href="{{ route('home') }}">Главная</a></li>
+                                        <li><a href="#">Исполнители</a></li>
+                                        <li><a href="#">Жанры</a></li>
+                    
+                                        <li><a href="{{ route('login') }}">Авторизация</a></li>
+                                       
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>             
+                        <div class="col-xl-2 col-lg-2 col-md-4">
+                            <div class="header-right-btn f-right d-none d-lg-block">
+                                <i class="fas fa-search special-tag"></i>
+                                <div class="search-box">
+                                    {{-- <form method="get" action="{{ route('search') }}">
+                                        <input type="text" name="s" value="" required>
+                                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                    </form> --}}
+                                    <form class="form-inline" method="get" action="{{ route('search') }}">
+                                        <input name="s" class="form-control mr-sm-2 " type="text" placeholder="Введите название" required="">
+                                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Найти</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-md-none"></div>
+                        </div>
                     </div>
-                    {{-- <a href="#"><div class="cart">
-                        <span class="cart_title">Cart(empty)</span>
-                        <img src="/public/assets/user/images/marker.png" alt=""/><div class="clear"></div>
-                   </div></a> --}}
                 </div>
-                <div class="clear"></div>
-            </div>
+           </div>
         </div>
-    </div>
-</div>
+   </div>
+    <!-- Header End -->
+</header>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,101 +1,105 @@
-<!DOCTYPE HTML>
-<html>
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>News HTML-5 Template </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="site.webmanifest">
+		<link rel="shortcut icon" type="image/x-icon" href="/public/assets/img/favicon.ico">
 
-<head>
-    <title>Free Musico Website Template | Home :: w3layouts</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="/public/assets/user/css/style.css" type="text/css" media="all" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <script src="/public/assets/user/js/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $(".dropdown img.flag").addClass("flagvisibility");
+		<!-- CSS here -->
+            <link rel="stylesheet" href="/public/assets/css/bootstrap.min.css">
+            <link rel="stylesheet" href="/public/assets/css/owl.carousel.min.css">
+            <link rel="stylesheet" href="/public/assets/css/ticker-style.css">
+            <link rel="stylesheet" href="/public/assets/css/flaticon.css">
+            <link rel="stylesheet" href="/public/assets/css/slicknav.css">
+            <link rel="stylesheet" href="/public/assets/css/animate.min.css">
+            <link rel="stylesheet" href="/public/assets/css/magnific-popup.css">
+            <link rel="stylesheet" href="/public/assets/css/fontawesome-all.min.css">
+            <link rel="stylesheet" href="/public/assets/css/themify-icons.css">
+            <link rel="stylesheet" href="/public/assets/css/slick.css">
+            <link rel="stylesheet" href="/public/assets/css/nice-select.css">
+            <link rel="stylesheet" href="/public/assets/css/style.css">
+   </head>
 
-            $(".dropdown dt a").click(function() {
-                $(".dropdown dd ul").toggle();
-            });
+   <body>
 
-            $(".dropdown dd ul li a").click(function() {
-                var text = $(this).html();
-                $(".dropdown dt a span").html(text);
-                $(".dropdown dd ul").hide();
-                $("#result").html("Selected value is: " + getSelectedValue("sample"));
-            });
-
-            function getSelectedValue(id) {
-                return $("#" + id).find("dt a span.value").html();
-            }
-
-            $(document).bind('click', function(e) {
-                var $clicked = $(e.target);
-                if (!$clicked.parents().hasClass("dropdown"))
-                    $(".dropdown dd ul").hide();
-            });
-
-
-            $("#flagSwitcher").click(function() {
-                $(".dropdown img.flag").toggleClass("flagvisibility");
-            });
-        });
-    </script>
-    <!--slider-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href="/public/assets/user/css/default.css" rel="stylesheet" type="text/css" media="all" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <script src="/public/assets/user/js/jquery-1.9.0.min.js"></script>
-    <script type="text/javascript">
-        $(window).load(function() {
-            $('#slider').nivoSlider();
-        });
-    </script>
-</head>
-
-<body>
     @include('layouts.header')
     
-    <div class="header-bottom ">
-        <div class="wrap">
-            <nav id="menu-wrap" style="padding: 13px 0 0px 0;">
-                <div class="row">
-                    <div class="col">
-                        <div class="d-flex">
-                        <div class="mx-auto">
-                <ul id="menu">
-                    <li><a href="index.html">Домой</a></li>
-                    <li>
-                        <a href="about.html">Каталог</a>
-                        <ul>
-                            <li><a href="about.html">Группы</a></li>
-                            <li><a href="about.html">Песни</a></li>
-                            <li><a href="about.html">Жанры</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="{{ route('login.create') }}">Авторизация</a></li>
-                    
-                </ul>
-            </div>
-        </div>
-    </div>
-            </div>
-            </nav>
-        </div>
-    </div>
-
     @yield('content')
-    <hr>
-
-<div class="footer">
-       
     
- <div class="copy-right">
-        <div class="wrap">
-            <p>&copy; 2013 Musico. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
-        </div>
-    </div>
-    <div class="clear"> </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-</div>
-</body>
 
-</html>
+    <footer>
+      
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="footer-border">
+                     <div class="row d-flex align-items-center justify-content-between">
+                         <div class="col-lg-6">
+                             <div class="footer-copy-right">
+                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                             </div>
+                         </div>
+                         <div class="col-lg-6">
+                             <div class="footer-menu f-right">
+                                 <ul>                             
+                                     <li><a href="#">Terms of use</a></li>
+                                     <li><a href="#">Privacy Policy</a></li>
+                                     <li><a href="#">Contact</a></li>
+                                 </ul>
+                             </div>
+                         </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer>
+    
+     <!-- JS here -->
+     
+         <!-- All JS Custom Plugins Link Here here -->
+         <script src=".//public/assets/js/vendor/modernizr-3.5.0.min.js"></script>
+         <!-- Jquery, Popper, Bootstrap -->
+         <script src=".//public/assets/js/vendor/jquery-1.12.4.min.js"></script>
+         <script src=".//public/assets/js/popper.min.js"></script>
+         <script src=".//public/assets/js/bootstrap.min.js"></script>
+         <!-- Jquery Mobile Menu -->
+         <script src=".//public/assets/js/jquery.slicknav.min.js"></script>
+ 
+         <!-- Jquery Slick , Owl-Carousel Plugins -->
+         <script src=".//public/assets/js/owl.carousel.min.js"></script>
+         <script src=".//public/assets/js/slick.min.js"></script>
+         <!-- Date Picker -->
+         <script src=".//public/assets/js/gijgo.min.js"></script>
+         <!-- One Page, Animated-HeadLin -->
+         <script src=".//public/assets/js/wow.min.js"></script>
+         <script src=".//public/assets/js/animated.headline.js"></script>
+         <script src=".//public/assets/js/jquery.magnific-popup.js"></script>
+ 
+         <!-- Breaking New Pluging -->
+         <script src=".//public/assets/js/jquery.ticker.js"></script>
+         <script src=".//public/assets/js/site.js"></script>
+ 
+         <!-- Scrollup, nice-select, sticky -->
+         <script src=".//public/assets/js/jquery.scrollUp.min.js"></script>
+         <script src=".//public/assets/js/jquery.nice-select.min.js"></script>
+         <script src=".//public/assets/js/jquery.sticky.js"></script>
+         
+         <!-- contact js -->
+         <script src=".//public/assets/js/contact.js"></script>
+         <script src=".//public/assets/js/jquery.form.js"></script>
+         <script src=".//public/assets/js/jquery.validate.min.js"></script>
+         <script src=".//public/assets/js/mail-script.js"></script>
+         <script src=".//public/assets/js/jquery.ajaxchimp.min.js"></script>
+         
+         <!-- Jquery Plugins, main Jquery -->	
+         <script src=".//public/assets/js/plugins.js"></script>
+         <script src=".//public/assets/js/main.js"></script>
+         
+     </body>
+ </html>
